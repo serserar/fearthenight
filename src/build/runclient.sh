de3d5d25596fa34473eac1a847e2a15077ffa35a
Sergio Serrano <serserar@gmail.com>
2020-02-15 11:43:35 +0100

    delete non src

--- src/build/runclient.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=/home/serserar/projects/cpp/undeadhunter/src/../data
-BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pushd ${DATA_DIR} >/dev/null
-${BIN_DIR}/undeadhunter $@
-popd >/dev/null
-
